d5f831b7774d0ece65feeb7d4546158297d765b9
roni4444 <debapriyopaul.dp@gmail.com>
2021-07-22 00:16:55 +0530

    basic layout mapping in home page

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def main_page():
     return render_template('index.html')
+
+
+@app.route('/achievements')
+def achieve_page():
+    return render_template('achievements.html')
+
+
+@app.route('/hobbies')
+def hobbies_page():
+    return render_template('hobbies.html')
 
 
 if __name__ == '__main__':
